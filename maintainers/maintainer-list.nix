21e2a09ff4b4cea1eeb6517250efa5aa49ad9dc2
Tim Schumacher <timschumi@gmx.de>
2026-05-23 13:51:07 +0200

    asdasd

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -868,12 +868,6 @@
     githubId = 209175;
     name = "Alesya Huzik";
   };
-  AhmedAmr = {
-    email = "ahmedamr24680@gmail.com";
-    github = "AhmedAmrNabil";
-    githubId = 43810060;
-    name = "Ahmed Amr";
-  };
   Ai-Ya-Ya = {
     email = "spg2500@gmail.com";
     github = "Ai-Ya-Ya";
